e47dd0f997c84ea7c80456d73ab50c14d7230ba9
Greg <pacome.oddgmail.com>
2017-01-12 16:20:24 +0100

    add application exceptions

---
+++ src/main/java/fr/formation/gestionColis/exception/DaoException.java
@@ -0,0 +1,27 @@
+package fr.formation.gestionColis.exception;
+
+public class DaoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DaoException() {
+		super();
+	}
+
+	public DaoException(final String message, final Throwable cause,
+			final boolean enableSuppression, final boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DaoException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public DaoException(final String message) {
+		super(message);
+	}
+
+	public DaoException(final Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/main/java/fr/formation/gestionColis/exception/DeleteEntityException.java
@@ -0,0 +1,28 @@
+package fr.formation.gestionColis.exception;
+
+public class DeleteEntityException extends DaoException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DeleteEntityException() {
+		super();
+	}
+
+	public DeleteEntityException(final String message, final Throwable cause,
+			final boolean enableSuppression, final boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DeleteEntityException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public DeleteEntityException(final String message) {
+		super(message);
+	}
+
+	public DeleteEntityException(final Throwable cause) {
+		super(cause);
+	}
+
+}
